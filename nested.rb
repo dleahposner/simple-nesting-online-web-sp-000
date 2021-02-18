dbd1c59d97b4586a5b03a755fea1d7d897783b56
Devorah Posner <dleahposner@gmail.com>
2021-02-18 22:19:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,9 @@ def adding_matz
         }
      }
      
-programmer_hash[:yukihiro_matsumoto] = [:known_for] = "Ruby", [:languages] = ["LiSP", "C"]
-programmer_hash
+# programmer_hash[:yukihiro_matsumoto] = {[:known_for] = "Ruby", [:languages] = ["LiSP", "C"]}
+
+# programmer_hash
     
 end
 
